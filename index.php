772e7eac6d43f609a3dc373a56fab2147d9185a4
talyab <talyab@users.noreply.github.com>
2015-02-15 19:27:52 +0200

    Create index.php
    First we start by creating a simple form and log in system.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+?>
+
+<form>
+
+  <input type="email" name="email" id="email" />
+  
+  <input type="password" name="password" />
+  
+  <input type="submit" value="Sign Up" />
+  
+</form>
